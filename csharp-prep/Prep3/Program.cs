005853f38e25103a00d8a4b7fadadc90f3af3b5e
Xavier Mitchell <xavieradammitchell@gmail.com>
2025-01-14 20:17:58 +0900

    Completed Prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,39 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        int number;
+        int guess;
+        int count = 0;
+        string run;
+        do
+        {
+            Console.Write("What's the magic number? ");
+            number = int.Parse(Console.ReadLine());
+            do
+            {
+
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+
+                count++;
+
+                if (guess > number)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if (guess < number)
+                {
+                    Console.WriteLine("Higher");
+                }
+            } while (guess != number);
+            Console.WriteLine($"You guessed it in {count} guesses!");
+
+            Console.Write("Do you want to play again? (yes/no): ");
+            run = Console.ReadLine();
+        } while (run == "yes");
     }
 }
