69779e72156c6c9bd9fa318321098761c25b39b3
Xavier Mitchell <xavieradammitchell@gmail.com>
2025-03-03 20:07:06 +0900

    Completed Learning05

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square square = new Square("pink", 2);
+        Console.WriteLine(square.GetColor());
+        Console.WriteLine(square.GetArea());
+
+        Rectangle rectangle = new Rectangle("purple", 3, 4);
+        Console.WriteLine(rectangle.GetColor());
+        Console.WriteLine(rectangle.GetArea());
+
+        Circle circle = new Circle("green", 5);
+        Console.WriteLine(circle.GetColor());
+        Console.WriteLine(circle.GetArea());
+
+        List<Shape> shapes = new() {square, rectangle, circle};
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"A {shape.GetColor()} shape with an area of {shape.GetArea()}.");
+        }
     }
 }
